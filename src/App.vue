61a2e6bd559a37ac493d53021b9c052144c20cc1
gsa143 <gsa143@sfu.ca>
2024-02-19 18:28:29 -0800

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,97 @@
 <template>
   <div class="app">
-    <p> {{ jobs[0].location }}</p>
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="heart" />
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
-import Job from './types/Job';
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobsList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
-  name: 'App',
-  components: {},
-  setup() {
-    //// Both reactive and refs can be used
-
-    // REACTIVE
-    // const state = reactive({
-    //   name: 'Link',
-    //   age: 25 as string | number
-    // })
-
-    
-    // return { ...toRefs(state)  }
-    
-    // REFERENCE
-    // const name = ref<string>('Link')
-    // const age = ref<number|string>(10)
-
-    // return { name, age }
-
-    const jobs = ref<Job[]>([
-      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-    ])
-
-    return { jobs }
+  name: "App",
+  components: {
+    JobList,
   },
-  // methods: {
-  //   changeName(name: string) {
-  //     this.name = name;
-  //     return name;
-  //   },
-  //   changeAge(age: string | number) {
-  //     this.age = age
-  //     return age;
-  //   }
-  // }
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
+  },
 });
 </script>
 
 <style>
-
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
+}
 </style>
